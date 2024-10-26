ba290ec723bcf733a4ba48eac714a589af6473a3
Mateus Rosa <mateusesr1@gmail.com>
2024-10-25 22:37:42 -0300

    Adicionado as demais Seeders corrigidos para facilitar os testes do app2

--- resources/views/client/edit.blade.php
+++ resources/views/client/edit.blade.php
@@ -7,7 +7,8 @@
     <title>Client Form</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
+    <style> 
+        
         body,
         html {
             height: 100%;
